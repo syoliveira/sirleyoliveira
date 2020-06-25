6aa623b73b33db7e6a5c12c71de35af168fb0a70
oliveirasirley <65969793+syoliveira@users.noreply.github.com>
2020-06-25 20:38:44 -0300

    Delete teste_lista.py

--- teste_lista.py
+++
@@ -1,9 +0,0 @@
-def funcao(aniversario):
-    aniversario = aniversario + 258
-    primos = [2]
-    for numerador in range(3, aniversario + 1):
-        for denominador in range(2, numerador):
-            if numerador % denominador == 0:
-                break
-        else: primos.append(numerador)
-    return primos
